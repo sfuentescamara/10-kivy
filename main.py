f1c2c8eb224d9218f59d57169fb23357f958ef83
sfuentescamara <s.fuentescamara@gmail.com>
2022-11-06 19:12:49 +0100

    version 1.1.0

--- main.py
+++ main.py
@@ -5,137 +5,176 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import StringProperty
 from random import seed, random, randint
 
+import json
+# 
 
-class Primera(Screen):
-  title = StringProperty("HOME")
-  text = StringProperty("Let's Go!")
-  text_hiden = StringProperty("")
-  index = -1
-  bool_show = False
+class Modo(Screen):
+    Screen = StringProperty("Lobby")
 
+    def changeScreen(where):
+        pass
 
-  allVerbs = {"sugerir"   :["arise",    "arose",        "arisen"],
-                  "ser"       :["be",       "was/were",     "been"  ], 
-                  "golpear"   :["beat",     "beat",         "beaten"  ], 
+class Lobby(Modo):
+    Screen = "Lobby"
 
-                  }
+class Grammar(Modo):
+    Screen = "Grammar"
+    title = StringProperty("HOME")
+    text = StringProperty("Let's Go!")
+    text_hiden = StringProperty("")
+    index = -1
+    bool_show = False
+    title_randomVerbs = StringProperty("Activate Randoms Verbs")
+    bool_randomVerbs = False
+    allFormsVerbs = {}
 
-  allFormsVerbs = {0   :["Present Simple"                    ,"Juego"                          ,"I play"],
-                   1   :["Present Continuous"                ,"Estoy jugando"                  ,"I'm playing"],
-                   2   :["Present Perfect Simple"            ,"He jugado"                      ,"I've played"],
-                   3   :["Present Perfect Continuous"        ,"He estado jugando"               ,"I've been playing"],
-                   4   :["Past Simple"                       ,"Jugé"                           ,"I played"],
-                   5   :["Past Continuous"                   ,"Estaba jugando"                 ,"I was playing"],
-                   6   :["Past Perfect Simple"               ,"Había jugado"                   ,"I had played"],
-                   7   :["Past Perfect Continuous"           ,"Había estado jugando"            ,"I had been playing"],
-                   8   :["Future Simple"                     ,"Jugaré"                         ,"I will play"],
-                   9   :["Future Continuous"                 ,"Estaré jugando"                 ,"I will be playing"],
-                   10  :["Future Perfect Simple"             ,"Habré jugado"                   ,"I will have played"],
-                   11  :["Future Perfect Continuous"         ,"Habré estado jugando"            ,"I will have been playing"],
-                   12  :["Be Going To Simple"                ,"Voy a jugar"                    ,"I'm going to play"],
-                   13  :["Be Going To Continuous"            ,"Voy a estar jugando"            ,"I'm going to be playing"],
-                   14  :["Future Past/Desire Simple"         ,"Jugaría"                        ,"I would play"],
-                   15  :["Future Past Perf/Desire Simple"    ,"Habría jugado"                  ,"I would have played"],
-                   16  :["Future Past Perf/Desire Continuous","Habría estado jugando"          ,"I would have been playing"],
-                   17  :["Modal verb High obligation"        ,"Debo jugar"                     ,"I must play"],
-                   18  :["Modal verb Med. obligation"        ,"Tengo que jugar"                ,"I have/need to play"],
-                   19  :["Modal verb Low obligation"         ,"Debería jugar"                  ,"I should play"],
-                   20  :["Modal verb Low Past obligation"    ,"Debería haber jugado"           ,"I should have played"],
-                   21  :["High probability"                  ,"Puedo jugar"                    ,"I may play"],
-                   22  :["Low probability"                   ,"Podría jugar"                   ,"I might play"],
-                   23  :["First Condicional"                 ,"",                              ,"If I play, I will improve \n\
-                                                                                                 If + subj. + present simple, \
-                                                                                                 subj. + will + infinitive"],
-                        }
+    verb = ["juego", "play", "played", "played", "playing"]
 
-  # rl_verbs = [x for x in allVerbs]
-  rl_formsVerbs = [x for x in allFormsVerbs]
+    with open("verbs.json") as verb_file:
+        allVerbs = json.load(verb_file)
 
-  def randomize(self, x):
-    if len(x) != 0:
-      random = randint(0, len(x)-1)
-      for i, item in enumerate(x):
-        if i == random:
-          # rl_verbs.pop(item)
-          self.rl_formsVerbs.pop(i)
-          return item
-    else:
-      self.title = "NICE JOB!"
-      self.text = ""
-      self.text_hiden = ""
-      self.rl_formsVerbs = [x for x in self.allFormsVerbs]
+    class DictVerb():
+        def functionVerb(self, verb):
+            self.allFormsVerbs = {0   :["Present Simple"                    ,"Juego"                          ,f"I {verb[1]}"],
+                    1   :["Present Continuous"                ,"Estoy jugando"                  ,f"I'm {verb[4]}"],
+                    2   :["Present Perfect Simple"            ,"He jugado"                      ,f"I've {verb[3]}"],
+                    3   :["Present Perfect Continuous"        ,"He estado jugando"              ,f"I've been {verb[4]}"],
+                    4   :["Past Simple"                       ,"Jugé"                           ,f"I {verb[2]}"],
+                    5   :["Past Continuous"                   ,"Estaba jugando"                 ,f"I was {verb[4]}"],
+                    6   :["Past Perfect Simple"               ,"Había jugado"                   ,f"I had {verb[3]}"],
+                    7   :["Past Perfect Continuous"           ,"Había estado jugando"           ,f"I had been {verb[4]}"],
+                    8   :["Future Simple"                     ,"Jugaré"                         ,f"I will {verb[1]}"],
+                    9   :["Future Continuous"                 ,"Estaré jugando"                 ,f"I will be {verb[4]}"],
+                    10  :["Future Perfect Simple"             ,"Habré jugado"                   ,f"I will have {verb[3]}"],
+                    11  :["Future Perfect Continuous"         ,"Habré estado jugando"           ,f"I will have been {verb[4]}"],
+                    12  :["Be Going To Simple"                ,"Voy a jugar"                    ,f"I'm going to {verb[1]}"],
+                    13  :["Be Going To Continuous"            ,"Voy a estar jugando"            ,f"I'm going to be {verb[4]}"],
+                    14  :["Future Past/Desire Simple"         ,"Jugaría"                        ,f"I would {verb[1]}"],
+                    15  :["Future Past Perf/Desire Simple"    ,"Habría jugado"                  ,f"I would have {verb[3]}"],
+                    16  :["Future Past Perf/Desire Continuous","Habría estado jugando"          ,f"I would have been {verb[4]}"],
+                    17  :["Modal verb High obligation"        ,"Debo jugar"                     ,f"I must {verb[1]}"],
+                    18  :["Modal verb Med. obligation"        ,"Tengo que jugar"                ,f"I have/need to {verb[1]}"],
+                    19  :["Modal verb Low obligation"         ,"Debería jugar"                  ,f"I should {verb[1]}"],
+                    20  :["Modal verb Low Past obligation"    ,"Debería haber jugado"           ,f"I should have {verb[3]}"],
+                    21  :["High probability"                  ,"Puedo jugar"                    ,f"I may {verb[1]}"],
+                    22  :["Low probability"                   ,"Podría jugar"                   ,f"I might {verb[1]}"],
+                    23  :["First Condicional *If*"            ,"Posibles situaciones futuras"   ,f"If I play, I will improve"],
+                    24  :["First Condicional *Even if*"       ,"Posibles situaciones futuras"   ,f"Even if I play, I won't improve"],
+                    # 25  :["Second Condicional *If*"           ,"Posibles situaciones futuras"   ,f"If I {verb[1]}, I will improve"],
+                    # 26  :["Second Condicional *Even if*"      ,"Posibles situaciones futuras"   ,f"Even if I {verb[1]}, I won't improve"],
+                    }
+            return self.allFormsVerbs
 
-    return -1
+    allFormsVerbs = DictVerb().functionVerb(verb)
+    rl_verbs = [x for x in allVerbs]
+    rl_formsVerbs = [x for x in allFormsVerbs]
 
-  def back(self):
-    self.index -= 1
-    if self.index < 0:
-      self.index = len(self.allFormsVerbs)-1
-    self.new(self.index)
+    def randomize(self, x):
+        if len(x) != 0:
+            random = randint(0, len(x)-1)
+            for i, item in enumerate(x):
+              if i == random:
+                    # rl_verbs.pop(item)
+                    x.pop(i)
+                    return item
+        else:
+            self.title = "NICE JOB!"
+            self.text = ""
+            self.text_hiden = ""
+            self.rl_formsVerbs = [x for x in self.allFormsVerbs]
+            self.rl_verbs = [x for x in self.allVerbs]
 
-  def next(self):
-    self.index += 1
-    if self.index > len(self.allFormsVerbs)-1:
-      self.index = 0
-    self.new(self.index)
+        return -1
 
-  def randomNext(self):
-    """
-        self.verb         set random verb and return a list
-        self.form_verb    set random from verbal and return ...
-    """
-    # self.verb = self.allVerbs[self.randomize(self.rl_verbs)]
-    id = self.randomize(self.rl_formsVerbs)
-    if id > 0:
-      self.new(id)
-    else:
-      pass
+    def back(self):
+        self.index -= 1
+        if self.index < 0:
+            self.index = len(self.allFormsVerbs)-1
+        self.new(self.index)
 
-  def new(self, select):
-    self.text_hiden = ""
-    self.bool_show = False
-    self.form_verb = self.allFormsVerbs[select]
-    self.title = self.form_verb[0]
-    self.text = self.form_verb[1]
+    def next(self):
+        self.index += 1
+        if self.index > len(self.allFormsVerbs)-1:
+            self.index = 0
+        self.new(self.index)
 
+    def randomNext(self):
+        id = self.randomize(self.rl_formsVerbs)
+        if(self.bool_randomVerbs):
+            self.id_verb = self.randomize(self.rl_verbs)
+            self.text = self.allVerbs[self.id_verb][0]
+            self.verb = self.allVerbs[self.id_verb]
+            self.allFormsVerbs = self.DictVerb().functionVerb(self.verb)
+        if id > 0:
+            self.new(id)
+        else:
+            pass
 
-  def show(self):
-    try:
-      if self.bool_show == False:
-        self.text_hiden = self.form_verb[2]
-        self.bool_show = True
-      else:
+    def new(self, select):
         self.text_hiden = ""
         self.bool_show = False
-    except:
-      pass
+        self.form_verb = self.allFormsVerbs[select]
+        self.title = self.form_verb[0]
+        if(not self.bool_randomVerbs):
+            self.text = self.form_verb[1]
 
-  def reset(self):
-    self.title = "HOME"
-    self.text = "Let's Go!"
-    self.text_hiden = ""
-    self.form_verb = ""
-    self.index = -1
-    self.bool_show = False
-    # self.rl_verbs = [x for x in allVerbs]
-    self.rl_formsVerbs = [x for x in self.allFormsVerbs]
+    def show(self):
+        try:
+            if self.bool_show == False:
+                self.text_hiden = self.form_verb[2]
+                self.bool_show = True
+            else:
+                self.text_hiden = ""
+                self.bool_show = False
+        except:
+            pass
 
-  def randomVerbs(self):
-    
-    bool_randomVerbs = True
+    def reset(self):
+        self.title = "HOME"
+        self.text = "Let's Go!"
+        self.text_hiden = ""
+        self.form_verb = ""
+        self.index = -1
+        self.bool_show = False
+        self.bool_randomVerbs = False
+        self.title_randomVerbs = "Desactivate Random Verbs"
+        self.rl_formsVerbs = [x for x in self.allFormsVerbs]
+        self.rl_verbs = [x for x in self.allVerbs]
 
+    def randomVerbs(self):
+        if self.bool_randomVerbs == False:
+            self.bool_randomVerbs = True
+            self.title_randomVerbs = "Desactivate Random Verbs"
+        else:
+            self.bool_randomVerbs = False
+            self.title_randomVerbs = "Activate Random Verbs"
+            self.verb = ["juego", "play", "played", "played", "playing"]
+
+
+
+
+class Vocabulary(Modo):
+    Screen = "Vocabulary"
+
+class WindowMain(ScreenManager):
+    title = "WindowMain"
+    text = StringProperty("Let's Go!")
+    text_hiden = StringProperty("")
+    index = -1
+    bool_show = False
 
 sm = ScreenManager()
-sm.add_widget(Primera(name="1"))
+sm.add_widget(Lobby(name="Lobby"))
+sm.add_widget(Grammar(name="Grammar"))
+sm.add_widget(Vocabulary(name="Vocabulary"))
 
 class Aplication(MDApp):
-  def build(self):
-    kv = Builder.load_file("new_window.kv")
-    seed()
-    
-    return kv
+    def build(self):
+        kv = Builder.load_file("test1.kv")
+        
+        return kv
 
 
 if __name__ == "__main__":
-  randomApp = Aplication()
-  randomApp.run()
+    randomApp = Aplication()
+    randomApp.run()
